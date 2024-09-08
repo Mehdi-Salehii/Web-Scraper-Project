a02f8e464bf580914b5109bb3c392718f3470056
Mehdi-Salehii <mehdisalehi9626@gmail.com>
2024-09-09 01:55:25 +0330

    getting prices for standard and premium working great now

--- getFullDetails.js
+++ getFullDetails.js
@@ -10,45 +10,87 @@ puppeteer.use(StealthPlugin());
 
 let finalResult = {};
 let firstTime = true;
+const checkCaptchaPresence = async (page) => {
+  const captchaExists = await page.evaluate(() => {
+    const pressHoldElement = document.querySelector(
+      '[aria-label="Press & Hold"]'
+    );
+    const pxCaptchaElement = document.getElementById("px-captcha");
 
-const extractPricesFromPage = async (page) => {
+    return !!pressHoldElement || !!pxCaptchaElement; // true if either exists
+  });
+
+  return captchaExists;
+};
+
+const navigateTo = async (page, url) => {
+  try {
+    console.log(`Navigating to ${url}...`);
+    await page.goto(url, { waitUntil: "domcontentloaded", timeout: 3 * 60000 });
+    console.log("Page loaded.");
+  } catch (error) {
+    console.error(`Error navigating to ${url}:`, error.message);
+    throw error;
+  }
+};
+const reactionToPage = async (
+  page,
+  captchaPresent,
+  clickCookies,
+  clickModal
+) => {
+  if (clickCookies)
+    await page
+      .click("#onetrust-reject-all-handler")
+      .catch((e) => console.error("Cookies handler not found", e));
+  if (clickModal)
+    await page
+      .click(".modal-content-close")
+      .catch((e) => console.error("Modal close button not found", e));
+  while (captchaPresent) {
+    console.log("there is a captcha present ❌");
+    if (clickCookies)
+      await page
+        .click("#onetrust-reject-all-handler")
+        .catch((e) => console.error("Cookies handler not found", e));
+    if (clickModal)
+      await page
+        .click(".modal-content-close")
+        .catch((e) => console.error("Modal close button not found", e));
+    await new Promise((res) => setTimeout(res, 1000 * 60));
+  }
+  console.log("passed from captcha ✅");
+  // if (captchaPresent) continue;
+};
+const extractPricesFromPage = async (
+  page,
+  captchaPresent,
+  clickCookies,
+  clickModal
+) => {
   const prices = { standard: [], premium: [] };
 
   try {
-    // Click the "package-tab-2" tab and extract prices
-    console.log("extracting prices for standard tier");
-    // await page.waitForSelector('label[for="package-tab-2"]', { visible: true });
-    await page.waitForNetworkIdle({ idleTime: 1000, timeout: 0 });
+    // Extract prices for the standard and premium tiers
+    console.log("waiting for package-tab-2");
+    // await reactionToPage(page, captchaPresent, clickCookies, clickModal);
+    await page.waitForSelector('label[for="package-tab-2"]', { visible: true });
+    // await reactionToPage(page, captchaPresent, clickCookies, clickModal);
     await page.click('label[for="package-tab-2"]');
     await page.waitForSelector("span.price-with-tooltip > span.price");
-    const priceSelectorStandard = await page.$(
-      "span.price-with-tooltip > span.price"
+    const standardPrices = await page.$$eval(
+      "span.price-with-tooltip > span.price",
+      (elements) => elements.map((e) => e.innerText.trim())
     );
-
-    const standardPrices = await page.evaluate(() => {
-      const priceElements = priceSelectorStandard;
-      return Array.from(priceElements).map((span) => span.innerText.trim());
-    });
-    console.log(`standard tier extracted prices ${standardPrices}`);
     prices.standard.push(...standardPrices);
-
-    // Click the "package-tab-3" tab and extract prices
-    console.log("extracting prices for premium tier");
-
-    // await page.waitForSelector('label[for="package-tab-3"]', { visible: true });
-    await page.waitForNetworkIdle({ idleTime: 1000, timeout: 0 });
+    // await reactionToPage(page, captchaPresent, clickCookies, clickModal);
     await page.click('label[for="package-tab-3"]');
+
     await page.waitForSelector("span.price-with-tooltip > span.price");
-    const priceSelectorPremium = await page.$(
-      "span.price-with-tooltip > span.price"
+    const premiumPrices = await page.$$eval(
+      "span.price-with-tooltip > span.price",
+      (elements) => elements.map((e) => e.innerText.trim())
     );
-
-    const premiumPrices = await page.evaluate(() => {
-      const priceElements = priceSelectorPremium;
-      return Array.from(priceElements).map((span) => span.innerText.trim());
-    });
-    console.log(`premium tier extracted prices ${premiumPrices}`);
-
     prices.premium.push(...premiumPrices);
   } catch (error) {
     console.error("Error extracting prices from page:", error.message);
@@ -56,35 +98,13 @@ const extractPricesFromPage = async (page) => {
 
   return prices;
 };
-
-const waitForPressHoldToDisappear = async (page) => {
-  let pressHoldExists = true;
-  let checkCounter = 0;
-  console.log("checking captcha presence");
-  while (pressHoldExists) {
-    // Ensure the page is fully loaded and idle
-    await page.waitForNetworkIdle({ idleTime: 1000, timeout: 0 }); // Remove timeout to wait indefinitely
-
-    // Check if the 'Press & Hold' element exists
-    pressHoldExists = await page.evaluate(() => {
-      return !!document.querySelector('[aria-label="Press & Hold"]');
-    });
-
-    if (pressHoldExists) {
-      checkCounter++;
-      console.log(
-        `Check ${checkCounter}: 'Press & Hold' element still present. Waiting 1 minute...`
-      );
-      // Wait for 1 minute before checking again
-      await new Promise((res) => setTimeout(res, 60000));
-    } else {
-      console.log("'Press & Hold' element has disappeared.");
-      break; // Exit loop when element is gone
-    }
-  }
-};
-
 const main = async (term, number) => {
+  let captchaPresent = false;
+  let clickModal = false;
+  let clickCookies = false;
+
+  // Define the interval to check for captcha presence
+
   console.log("Starting Puppeteer...");
 
   try {
@@ -92,38 +112,39 @@ const main = async (term, number) => {
       headless: false,
       executablePath: executablePath(),
     });
-    // process.env.PUPPETEER_EXECUTABLE_PATH ||
-    //       "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
     const page = await browser.newPage();
     const userAgent = randomUserAgent.getRandom();
     await page.setUserAgent(userAgent);
     await page.setExtraHTTPHeaders({ "Accept-Language": "en-US,en;q=0.9" });
 
-    const navigateTo = async (url) => {
-      try {
-        console.log(`Navigating to ${url}...`);
-        await page.goto(url, { waitUntil: "networkidle0", timeout: 3 * 60000 });
-        console.log("Page loaded.");
-      } catch (error) {
-        console.error(`Error navigating to ${url}:`, error.message);
-        throw error;
-      }
-    };
-
     console.log(`Scraping results for "${term}"...`);
     const url = `https://www.fiverr.com/search/gigs?query=${encodeURIComponent(
       term
     )}&page=${number}`;
-    await navigateTo(url);
-    // await waitForPressHoldToDisappear(page);
-    const pressHoldElement = await page.$('[aria-label="Press & Hold"]');
-    if (pressHoldElement) {
-      console.log(
-        "Element found, possibly need to handle press & hold interaction"
-      );
-    } else {
-      console.log("No Press & Hold element found");
-    }
+    // const checkCaptchaInterval = setInterval(async () => {
+    //   const result = await page.evaluate(() => {
+    //     return {
+    //       captchaPresent: !!document.querySelector(
+    //         '[aria-label="Press & Hold"]' ||
+    //           !!document.getElementById("px-captcha")
+    //       ),
+    //       clickCookies: !!document.querySelector("#onetrust-banner-sdk"),
+    //       clickModal: !!document.querySelector(".modal-content"),
+    //     };
+    //   });
+
+    //   // Update the variables
+    //   captchaPresent = result.captchaPresent;
+    //   clickCookies = result.clickCookies;
+    //   clickModal = result.clickModal;
+
+    //   // Log to debug
+    //   console.log(
+    //     `captchaPresent: ${captchaPresent}, clickCookies: ${clickCookies}, clickModal: ${clickModal}`
+    //   );
+    // }, 1000);
+    await navigateTo(page, url);
+    // await reactionToPage(page, captchaPresent, clickCookies, clickModal);
     // Extract hrefs from gig cards
     console.log(`extracting hrefs...`);
     let hrefs = await page.evaluate(() => {
@@ -133,8 +154,8 @@ const main = async (term, number) => {
         ),
       ].map((anchor) => anchor.href);
     });
+
     hrefs = hrefs.length ? [...new Set(hrefs)] : [];
-    console.log(hrefs);
     console.log(
       `${hrefs.length ? `✅ hrefs extracted` : `❌ hrefs extraction failed`}`
     );
@@ -142,37 +163,26 @@ const main = async (term, number) => {
     for (let i = 0; i < hrefs.length; i++) {
       console.log(`running for ${i + 1} href of ${hrefs.length} hrefs`);
       const href = hrefs[i];
-      // Navigate to gig detail page in the same tab
-      await navigateTo(href);
+      await navigateTo(page, href);
+      while (await checkCaptchaPresence(page)) {
+        console.log("captcha is present waiting for 20 seconds ");
+        await new Promise((res) => setTimeout(res, 20000));
+      }
+      // await reactionToPage(page, captchaPresent, clickCookies, clickModal);
 
-      const { skipCheck, clickModal, clickCookies } = await page.evaluate(
-        () => {
-          return {
-            skipCheck: !!document.querySelector('[aria-label="Press & Hold"]'),
-            clickCookies: !!document.querySelector("#onetrust-banner-sdk"),
-            clickModal: !!document.querySelector(".modal-content"),
-          };
-        }
-      );
-      if (skipCheck) continue;
-      clickCookies && (await page.click("#onetrust-reject-all-handler"));
-      clickModal && (await page.click(".modal-content-close"));
-      // await waitForPressHoldToDisappear(page);
-      await page.waitForNetworkIdle({ idleTime: 1000, timeout: 0 });
-      const packageContent = page.$(".package-content");
-      if (!packageContent) continue;
-      // Wait for 'Press & Hold' to disappear, if applicable
-      console.log(`evaluating press and hold presence`);
+      const packageContentExists = await page.$(".package-content");
+      if (!packageContentExists) continue;
 
-      // Extract prices from the gig detail page
       console.log(`getting results for gig`);
-      const { standard, premium } = await extractPricesFromPage(page);
+      const { standard, premium } = await extractPricesFromPage(
+        page,
+        captchaPresent,
+        clickCookies,
+        clickModal
+      );
 
-      // Go back to the search results page
-      console.log(`rolling back to run next href...`);
-      await page.goBack({ waitUntil: "networkidle0" });
+      await page.goBack({ waitUntil: "domcontentloaded" });
 
-      // Store the extracted prices
       if (standard.length) {
         finalResult[term] = finalResult[term] || {};
         finalResult[term][0] = (finalResult[term][0] || []).concat(standard);
@@ -189,29 +199,26 @@ const main = async (term, number) => {
       `Extracted prices from page ${number} for "${term}":`,
       finalResult[term]
     );
-
+    await writeObjectToFile(finalResult);
+    await report();
     await browser.close();
     console.log("Browser closed.");
+    // clearInterval(checkCaptchaInterval);
   } catch (error) {
     console.error("Overall Error:", error.message);
+    // clearInterval(checkCaptchaInterval);
   }
 };
 
-const terms = [
-  `fullstack`,
-  `fullstack app`,
-  `fullstack nextjs`,
-  `fullstack developer`,
-];
-
+const terms = [`fullstack app`];
+// `fullstack nextjs`,
+//   `fullstack developer`,
 const runLoop = async () => {
   for (const term of terms) {
-    for (const i of [1, 2]) {
+    for (const i of [2]) {
       await main(term, i);
     }
   }
-  await writeObjectToFile(finalResult);
-  await report();
 };
 
 runLoop();
